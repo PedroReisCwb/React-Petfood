c25fc2ed50f091a8b474074f12e74a2dd4060981
Pedro Reis <reis.cwb@hotmail.com>
2021-01-05 07:38:27 -0300

    Populando banco

--- api/src/data/addPetshopAndProducts.js
+++ api/src/data/addPetshopAndProducts.js
@@ -8,7 +8,7 @@ require('../database');
 const addPetshopsAndProducts = async () => {
     try {
         for (let petshop of petshops) {
-            const newPetshop = await new Petshop(Petshop).save();
+            const newPetshop = await new Petshop(petshop).save();
             await Product.insertMany(
                 petshop.produtos.map((p) => ({ ...p, petshop_id: newPetshop._id }))
             );
@@ -19,3 +19,5 @@ const addPetshopsAndProducts = async () => {
         console.log(err.message);
     }
 }
+
+addPetshopsAndProducts();
